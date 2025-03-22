8ef8b3af226ffc4176afa09720d75faf1d3c85a5
matsubaraDaisuke <matsunenenkao@gmail.com>
2025-03-22 16:10:01 +0900

    test

--- README.md
+++
@@ -1,3 +0,0 @@
-# flask-mnist-app
-# flask-mnist-app
-# flask-mnist-app
--- mnist.py
+++ mnist.py
@@ -15,10 +15,13 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
 
+# app.secret_key = "your_secret_key_here"  
+# submitボタンを押した際にエラーが出た場合上の行のコメントアウトを削除し、your_secret_key_hereに任意の文字列（例:aidemy)を指定し、再度アプリケーションを実行してください。
+
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-model = load_model('./model.h5', compile=False)#学習済みモデルをロード
+model = load_model('./model.keras')#学習済みモデルをロード
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -37,7 +40,7 @@ def upload_file():
             filepath = os.path.join(UPLOAD_FOLDER, filename)
 
             #受け取った画像を読み込み、np形式に変換
-            img = image.load_img(filepath, grayscale=True, target_size=(image_size,image_size))
+            img = image.load_img(filepath, color_mode='grayscale', target_size=(image_size,image_size))
             img = image.img_to_array(img)
             data = np.array([img])
             #変換したデータをモデルに渡して予測する
@@ -51,5 +54,4 @@ def upload_file():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host ='0.0.0.0',port = port)
+    app.run()
